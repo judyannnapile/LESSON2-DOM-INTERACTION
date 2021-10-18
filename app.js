7fcdfc64a90b2fc3644bd8b874d646c1281d5d19
Judy Ann J. Napile <79519742+judyannnapile@users.noreply.github.com>
2021-10-18 08:14:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+data(){
+    return{
+        num1:'',
+        num2:'',
+        name:'Judy Ann Napile',
+        age:22,
+        some image:'https://images.app.goo.gl/SqHrGZG76qZijUoG8',
+    };
+},
+methods:{
+    setName(Data){
+        this.name=Data.target.value;
+    },
+    after5(num){
+        const totalage=this.age+num
+        return totalage
+    },
+    myfav(num1,num2){
+        const randomnum = Math.random
+        (num1,num2)
+        return randomnum
+    }
+
+}
+
+});
+app.mount('#Data');
